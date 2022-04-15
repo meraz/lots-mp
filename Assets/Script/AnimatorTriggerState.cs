4368efbc4ae1a9cfe1d6b58f64c6b4cfd30458bd
Rasmus Tilljander <git@rasmus.tilljander.eu>
2022-04-15 11:13:29 +0200

    Sync float triggers over network, works well :)

--- Assets/Script/AnimatorTriggerState.cs
+++ Assets/Script/AnimatorTriggerState.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using UnityEngine.Assertions;
 using System.Collections.Generic;
+using Unity.Netcode;
 
 namespace Lots
 {
-    public class AnimatorTriggerState : MonoBehaviour
+    public class AnimatorTriggerState : NetworkBehaviour
     {
         public Animator animator;
 
@@ -13,7 +14,7 @@ namespace Lots
 
         void Awake()
         {
-            if(!animator)
+            if (!animator)
             {
                 animator = GetComponent<Animator>();
                 Assert.IsNotNull(animator, "Animation cannot be null.");
@@ -22,14 +23,77 @@ namespace Lots
 
         public void SetFloat(string name, float value)
         {
+            if (!IsOwner)
+            {
+                return;
+            }
+
+            // TODO check if historical value is same
             floatTriggers[name] = value;
+
             animator.SetFloat(name, value);
+
+            if (!IsHost)
+            {
+                SetFloatTriggerServerRpc(name, value);
+            }
+            else
+            {
+                SetFloatTriggerClientRpc(name, value);
+            }
         }
 
         public void SetBool(string name, bool value)
         {
+            if (!IsOwner)
+            {
+                return;
+            }
+
+            // TODO check if historical value is same
             boolTriggers[name] = value;
             animator.SetBool(name, value);
+
+            if (!IsHost)
+            {
+                SetBoolTriggerServerRpc(name, value);
+            }
+            else
+            {
+                SetBoolTriggerClientRpc(name, value);
+            }
+        }
+
+        [ServerRpc]
+        public void SetFloatTriggerServerRpc(string name, float value)
+        {
+            SetFloatTriggerClientRpc(name, value);
+        }
+
+        [ClientRpc(Delivery = RpcDelivery.Unreliable)]
+        public void SetFloatTriggerClientRpc(string name, float value)
+        {
+            if (!IsOwner)
+            {
+                // Only overwrite data if you're not the owner, the owner will have the value already set.
+                animator.SetFloat(name, value);
+            }
+        }
+
+        [ServerRpc]
+        public void SetBoolTriggerServerRpc(string name, bool value)
+        {
+            SetBoolTriggerClientRpc(name, value);
+        }
+
+        [ClientRpc(Delivery = RpcDelivery.Unreliable)]
+        public void SetBoolTriggerClientRpc(string name, bool value)
+        {
+            if (!IsOwner)
+            {
+                // Only overwrite data if you're not the owner, the owner will have the value already set.
+                animator.SetBool(name, value);
+            }
         }
     }
 }
--- Assets/Script/Network/NetworkAnimatorTrigger.cs
+++ Assets/Script/Network/NetworkAnimatorTrigger.cs
@@ -1,17 +1,22 @@
 using UnityEngine.Assertions;
 using Unity.Netcode;
+using UnityEngine;
+using System.Collections.Generic;
 
 namespace Lots
 {
     public class NetworkAnimatorTrigger : NetworkBehaviour
     {
-        AnimatorTriggerState animatorTriggerState;
+        private AnimatorTriggerState animatorTriggerState; // Origin data to read by owner
+        private Animator animator; // Target data data to write by client
 
         void Awake()
         {
             animatorTriggerState = GetComponent<AnimatorTriggerState>();
             Assert.IsNotNull(animatorTriggerState, "AnimatorTriggerState cannot be null.");
-        }
 
+            animator = GetComponent<Animator>();
+            Assert.IsNotNull(animator, "Animator cannot be null.");
+        }
     }
 }
